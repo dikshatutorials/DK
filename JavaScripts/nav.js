d06fe67363ffa9a81475eb507557dd98d2f46193
Vivek Thakur <20509.vivek@gpranchi.org>
2023-10-17 22:48:12 +0530

    commit

---
+++ JavaScripts/nav.js
@@ -0,0 +1,9 @@
+
+function openNav() {
+    document.getElementById("menu").style.display = 'block';
+    document.getElementById("menu").style.width = '100%';
+}
+function closeNav() {
+    document.getElementById("menu").style.width = '0%';
+    document.getElementById("menu").style.display = 'none';
+}
