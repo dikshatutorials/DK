ab4c0c9b1d5493d5953625bf1ea21fab236c5957
Vivek Thakur <20509.vivek@gpranchi.org>
2024-01-08 08:24:29 +0530

    commit

---
+++ coaching-site/src/lib/About.svelte
@@ -0,0 +1,87 @@
+<script>
+  import sir_pic from "../assets/images/sir.jpg";
+</script>
+
+<div class="about">
+  <div class="abt-imag">
+    <img src={sir_pic} alt="sir" />
+  </div>
+  <div class="abt-text">
+    <h1>About Us</h1>
+    <h4>
+      Since the inception of our coaching center, our unwavering commitment has
+      always been to deliver excellence in education. With a steadfast
+      dedication to our mission, we strive to empower students to reach their
+      highest potential, thereby enabling them to become the finest versions of
+      themselves. Our overarching goal is not only to impart knowledge but also
+      to instill values that transcend academic achievements.
+      <br />
+
+      At the core of our philosophy lies the belief that education is a
+      transformative force capable of shaping not only individual destinies but
+      also the collective destiny of our world. We firmly believe that each
+      student holds the key to making a positive impact on society, and through
+      our guidance, we aspire to nurture the qualities essential for such
+      contributions.
+
+      <br />
+      Our motto is not merely a slogan; it's a driving force that propels us forward
+      each day. We understand that the dreams of our students are the seeds of a
+      better, more harmonious world. By fostering an environment that encourages
+      curiosity, critical thinking, and creativity, we aim to equip our students
+      with the tools necessary to make meaningful contributions to society.
+
+      <br />
+      As we navigate the ever-evolving landscape of education, we remain committed
+      to staying at the forefront of innovative teaching methodologies and curricula.
+      We believe in adapting to the changing needs of our students and the dynamic
+      global landscape, ensuring that our educational offerings remain relevant and
+      effective.
+    </h4>
+  </div>
+</div>
+
+<style lang="css3">
+  .about {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    background: #952c4a;
+  }
+  .abt-imag {
+    width: 40%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .abt-imag img {
+    width: 110%;
+    height: 110%;
+  }
+  .abt-text {
+    padding: 1rem 1rem 0 1rem;
+    color: #ffffffea;
+  }
+  .abt-text h1 {
+    font-size: 2rem;
+    font-weight: 400;
+    margin-left: 5%;
+  }
+  .abt-text h1::after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 1px;
+    background: #ffffff7e;
+    margin: 1rem 0;
+  }
+
+  .abt-text h4 {
+    font-size: 1rem;
+    font-weight: 300;
+    margin-left: 5%;
+  }
+</style>
---
+++ coaching-site/src/lib/Home.svelte
@@ -0,0 +1,77 @@
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+  crossorigin="anonymous"
+></script>
+
+<div class="home">
+    <div class="home-text-part">
+        <h1>Diksha Tutorials Coaching Center</h1>
+        <h4>
+            One of the best coaching center in the city. We provide the best
+            education to the students. We have the best teachers in the city.
+        </h4>
+
+        <a href="*">
+            <button class="btn">Know More</button>
+        </a>
+    </div>
+</div>
+
+<style lang="css3">
+    .home {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background: rgba(0,0,0,0.5) url("../assets/images/bg.jpeg") no-repeat center center/cover;
+        background-blend-mode: darken;
+    }
+    .home-text-part {
+        width: 50%;
+        height: 50%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .home-text-part h1 {
+        color: #ffffff;
+        font-size: 3rem;
+        font-weight: 500;
+    }
+    .home-text-part h1::after {
+        content: "";
+        display: block;
+        width: 100%;
+        height: 3px;
+        background: #ffffffa1;
+        margin: 20px auto;
+    }
+
+    .home-text-part h4 {
+        color: #ffffffa2;
+        font-size: 1.5rem;
+        font-weight: 400;
+        text-align: center;
+    }
+    .home-text-part .btn {
+        margin-top: 20px;
+        background: #ffffffa2;
+        border-radius: .2rem !important;
+        width:150px;
+    }
+    @media screen and (max-width: 680px) {
+        .home-text-part {
+            width: 80%;
+            height: 80%;
+        }
+        .home-text-part h1{
+            font-size: 2rem;
+        }
+        .home-text-part h4{
+            font-size: 1rem;
+            text-align: left;
+        }
+    }
+</style>
